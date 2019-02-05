7fc92c59e62483c1cdda94fe0d72b2af8e4b742f
srscalia <shelby.scalia@gmail.com>
2019-02-05 12:18:55 -0500

    update about me

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
           <div className="AboutContainer">
             <div id="aboutText">
               <p>A Virginian at heart, I moved to NYC after graduating from <a href="https://www.wm.edu/" target="_blank" rel="noopener noreferrer">William & Mary</a>, a small liberal arts school in Williamsburg, VA. While working as a project management analyst, I tried coding for the first time. Soon after, I quit my job and applied to the <a href="https://flatironschool.com/" target="_blank" rel="noopener noreferrer">Flatiron School</a> to pursue learning to code fulltime. For more info, head over to <a href="https://linkedin.com/in/shelby-scalia" target="_blank" rel="noopener noreferrer">my LinkedIn profile</a>.</p>
-              <p>Having completed Flatiron's 15-week <a href="https://flatironschool.com/career-courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">Immersive Software Engineering Bootcamp</a>, I am excited by the idea of solving challenging problems for a company in the fashion, beauty and/or e-commerce space as junior software engineer.</p>
+              <p>Having completed Flatiron's 15-week <a href="https://flatironschool.com/career-courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">Immersive Software Engineering Bootcamp</a>, I am looking to work on challenging problems in a fast-paced environment alongside intellectually curious peers.</p>
             </div>
             <div id="aboutDownArrow">
               <div onClick={()=>this.props.portfolioClick()} className='arrow'><FontAwesomeIcon icon="caret-down"/></div>
@@ -23,7 +23,7 @@ class About extends Component {
           <div className="AboutContainerResponsive">
             <div id="aboutTextResponsive">
               <p>A Virginian at heart, I moved to NYC after graduating from <a href="https://www.wm.edu/" target="_blank" rel="noopener noreferrer">William & Mary</a>, a small liberal arts school in Williamsburg, VA. While working as a project management analyst, I tried coding for the first time. Soon after, I quit my job and applied to the <a href="https://flatironschool.com/" target="_blank" rel="noopener noreferrer">Flatiron School</a> to pursue learning to code fulltime. For more info, head over to <a href="https://linkedin.com/in/shelby-scalia" target="_blank" rel="noopener noreferrer">my LinkedIn profile</a>.</p>
-              <p>Having completed Flatiron's 15-week <a href="https://flatironschool.com/career-courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">Immersive Software Engineering Bootcamp</a>, I am excited by the idea of solving challenging problems for a company in the fashion, beauty and/or e-commerce space as junior software engineer.</p>
+            <p>Having completed Flatiron's 15-week <a href="https://flatironschool.com/career-courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">Immersive Software Engineering Bootcamp</a>, I am looking to work on challenging problems in a fast-paced environment alongside intellectually curious peers.</p>sh
             </div>
           </div>
         </MediaQuery>
